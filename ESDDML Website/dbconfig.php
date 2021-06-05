0c846ba5df3b61c1f13c7a3a790b34cd8eee2346
Viraj Shah <virajtshah1999@gmail.com>
2021-06-05 10:58:48 +0530

    Add files via upload

---
+++ ESDDML Website/dbconfig.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("localhost","id16509929_nesddml","VitDyslexia1@","id16509929_ddc");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
---
+++ ESDDML Website/thanks.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Dyslexia Data Collection</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="shortcut icon" type="image/png" href="image/logo.png" />
+        
+    </head>
+    <body>
+        <center>
+            <div class="intro">
+                <h1> ESDDML </h1>
+                <h3>Thanks for giving your time and response and please fill the form only once</h3>
+                <a href="welcome.php" class="btn"> Home Page </a> 
+            </div>
+        </center>
+    </body>
+</html>
